992440f2d35c29fc9d64e3a29ea6b6a3b37d30fe
Abdullatifsadiq <Abdul.latif708@yahoo.com>
2024-02-14 08:44:11 +0000

    laying out code structure

---
+++ inventory_system.py
@@ -0,0 +1,111 @@
+"""An inventory system that does all the things listed below
+
+# Steps
+# Create item
+# Add item to inventory
+# Find item
+# update item
+# Delete item
+# Purchase item and update inventory and till balance
+# Return item and update inventory and till balance
+
+"""
+
+import json
+
+
+def save_items(items):
+    """Save items into a json file"""
+
+    with open("items.json", "w", encoding="utf-8") as file:
+        json.dump(items, file)
+
+
+def load_items():
+    """Loading saved items from a json file"""
+    try:
+        with open("items.json", "r", encoding="utf-8") as file:
+            return json.load(file)
+
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
+
+
+class Item:
+    """Class for creating an item"""
+
+    def __init__(self, name, quantity, price):
+        self.name = name
+        self.quantity = quantity
+        self.price = price
+
+    def item_details(self):
+        """Function to gather the item details into a dictionary"""
+
+        item_dict = {"Name": self.name, "Quantity": self.quantity, "Price": self.price}
+        return item_dict
+
+    # def add_item(item, item_list):
+    # """Function to add an item to list"""
+
+    # it_details = item.item_details()
+    # item_list.append(it_details)
+    # save_items(item_list)
+
+
+print("\nchoose an option")
+print("1. Business Owner")
+print("2. Customer")
+print("3. Exit program")
+
+first_choice = int(input("Are you a Business Owner or a Customer  "))
+
+
+if first_choice == 1:
+    print("\nchoose an option")
+    print("1. Add a new item")
+    print("2. Find an item")
+    print("3. Update an item")
+    print("4. Delete an item")
+    print("5. Back")
+
+    choice = int(input("Enter option 1,2,3,4 or 5 to proceed  "))
+
+    if choice == 1:
+        print("Add a new item")
+
+    elif choice == 2:
+        print("Find an item")
+
+    elif choice == 3:
+        print("Update an item")
+
+    elif choice == 4:
+        print("Delete an item")
+
+    else:
+        print("\nchoose an option")
+        print("1. Business Owner")
+        print("2. Customer")
+        print("3. Exit program")
+
+
+else:
+    print("\nchoose an option")
+    print("1. Purchase an item")
+    print("2. Return an item")
+    print("3. Back")
+
+    choice = int(input("Enter option 1,2 or 3  "))
+
+    if choice == 1:
+        print("Purchase an item")
+
+    elif choice == 2:
+        print("Return an item")
+
+    else:
+        print("\nchoose an option")
+        print("1. Business Owner")
+        print("2. Customer")
+        print("3. Exit program")
