c34227cfce8a018edb8081f0d92ee3279f45fad5
8adiq <abdul.latif708@yahoo.com>
2024-05-12 14:56:30 +0000

    adding and shwoing item done

--- inventory.py
+++ inventory.py
@@ -1,29 +1,67 @@
 import csv
 import re
+import json
 
 
-class Item():
-    def __init__(self,name,price,quantity):
-        self.name = name
-        self.price = price
-        self.quantity = quantity
-        # self.save_items()
+items = []
+class Inventory():
+
+
+    def __init__(self):
+        self.inventory = {}
+        self.filename = 'items.csv'
 
     def save_items(self):
-            with open('items.csv', 'a') as file:
+            with open(self.filename, 'a') as file:
                 writer = csv.DictWriter(file,fieldnames=['Name','Price','Quantity'])
-                writer.writerow=({'Name':self.name,'Price':self.price,'Quantity':self.quantity})
+                if file.tell() == 0: # makes sure the file is empty before creating the header row
+                    writer.writeheader()
+                for item in self.inventory.values():
+                    writer.writerow({'Name':item['Name'],'Price':item['Price'],'Quantity':item['Quantity']})
 
     def load_items(self):
-        items = []
         try:
-            with open('items.csv','r') as file:
+            with open(self.filename,'r') as file:
                 reader = csv.DictReader(file)
                 for row in reader:
                     items.append({'Name':row['Name'], 'Price':row['Price'], 'Quantity':row['Quantity']})
         except FileNotFoundError:
             print("No Inventory file found.")
-        return items
+        
+
+    def add_item(self,name,price,quantity):
+        ...
+        self.inventory[name] = {'Name':name,'Price':price,'Quantity':quantity}
+        print(f'{name} added')
+        self.save_items()
+
+    
+    def find_item():
+        ...
+
+    def update_item():
+        ...
+    
+    def delete_item():
+        ...
+
+    def show_all_item(self):
+        self.load_items()
+
+        if items:
+            for item in items:
+                print(f"{item['Name']} {item['Price']} {item['Quantity']}")
+
+        else:
+            print("Inventory is currently empty. Add a new item.")
+
+
+    def purchase_item():
+        ...
+
+    def return_item():
+        ...
+
     
     def after_purchase(self,amt_bought):
         if amt_bought <= self.quantity:
@@ -42,38 +80,11 @@ class Item():
         return f'Name : {self.name}, Price: {self.price}, Quantity: {self.quantity}' 
 
 
-class Inventory():
-    
-    items = []
-
-    def add_item(cls,item):
-        item = Item()
-        cls.items.append(item)
-        item.save_items()
-    
-    def find_item():
-        ...
-
-    def update_item():
-        ...
-    
-    def delete_item():
-        ...
-
-    def show_all_item():
-        ...
-
-    def purchase_item():
-        ...
-
-    def return_item():
-        ...
-
-    
-
-
 
 def main():
+
+    inventory = Inventory()
+    # inventory.load_items()
 
     print('\nChoose an option')
     print('1. Business Owner')
@@ -100,8 +111,8 @@ def main():
             name = input("Name : ")
             price = input("Price : ")
             quantity = input("Quantity : ")
-            item = Item(name,price,quantity)
-            print(item)
+
+            inventory.add_item(name,price,quantity)
             
         elif choice == 2:
             ...
@@ -111,6 +122,7 @@ def main():
         
         elif choice == 4:
             ...
+            inventory.show_all_item()
 
         elif choice == 5:
             ...
